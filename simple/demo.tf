0c69d029bb1bfd95b8ea550c848b216ddd29b4e2
Ali altaher <ali.altaher@external.thalesgroup.com>
2023-10-26 13:56:23 +0400

    update instance name

--- simple/demo.tf
+++ simple/demo.tf
@@ -7,4 +7,7 @@ provider "aws" {
 resource "aws_instance" "ec2_terraform1" {
     ami = "ami-0dbc3d7bc646e8516"
     instance_type = "t2.micro"
+    tags = {
+      "Name" = "ec2_terraform1"
+    }
 }
