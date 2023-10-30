b72c9b26f8c13a67039e1b061755c8f18420be1a
Ali altaher <ali.altaher@external.thalesgroup.com>
2023-10-30 16:22:02 +0400

    adding variables

---
+++ variables/variables.tf
@@ -0,0 +1,3 @@
+variable "vpn_IP" {
+    default = "116.50.30.20/32"
+}
---
+++ variables/varsdemo.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_security_group" "sec1" {
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol= "tcp"
+        cidr_blocks = [var.vpn_IP]
+    }
+
+}
+resource "aws_security_group" "sec2" {
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol= "tcp"
+        cidr_blocks = [var.vpn_IP]
+    }
+
+}
+resource "aws_security_group" "sec3" {
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol= "tcp"
+        cidr_blocks = [var.vpn_IP]
+    }
+
+}
