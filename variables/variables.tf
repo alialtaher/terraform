856ec4a876f265693982ecdc3646b66a7fa784b0
Ali altaher <ali.altaher@external.thalesgroup.com>
2023-11-01 11:04:04 +0400

    using terraform.tf

--- variables/variables.tf
+++ variables/variables.tf
@@ -1,3 +1,21 @@
-variable "vpn_IP" {
+#default variables
+
+variable "vpn_IP" { ###to be used in varsdemo, sec1, sec2, and sec3 
     default = "116.50.30.20/32"
-}
+} 
+
+variable "instancetype" { ###to be used in varsdemo, instance1 
+    default = "t2.micro"
+} 
+variable "goldenami" { ###to be used in varsdemo, instance1 
+    default = "ami-124356722"
+} 
+
+#default value can be override by env vars, e.g: terraform plan -var "instance_type=t2.small"
+# if no default value is set, The cli will ask for a value of variable
+
+#alternatively, you can specify the variables in .tfvars file 
+
+# The variable reference priority is to check if terraform.tfvars is available, Then to check in the tf files, If nothing found then it will be based on STDIN
+# you can specify a custom var file using -var-file option 
+# you can use export TF_VAR_NAMEOFVAR="", it will be inherited automatically in the next execution plan  
--- variables/varsdemo.tf
+++ variables/varsdemo.tf
@@ -7,7 +7,8 @@ resource "aws_security_group" "sec1" {
         from_port = 443
         to_port = 443
         protocol= "tcp"
-        cidr_blocks = [var.vpn_IP]
+    #    cidr_blocks = [var.vpn_IP]
+        cidr_blocks = [var.vpn_IP]        
     }
 
 }
@@ -16,6 +17,7 @@ resource "aws_security_group" "sec2" {
         from_port = 80
         to_port = 80
         protocol= "tcp"
+    #    cidr_blocks = [var.vpn_IP]
         cidr_blocks = [var.vpn_IP]
     }
 
@@ -25,7 +27,14 @@ resource "aws_security_group" "sec3" {
         from_port = 8080
         to_port = 8080
         protocol= "tcp"
+    #    cidr_blocks = [var.vpn_IP]
         cidr_blocks = [var.vpn_IP]
     }
 
+}
+resource "aws_instance" "instance1" {
+  ami = var.goldenami
+  #instance_type = var.instancetype
+  instance_type = var.instancetype
+  
 }
