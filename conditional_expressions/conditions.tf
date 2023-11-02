2e4a99d1c5d16495f62b4c36348e33a91e2dc94f
Ali altaher <ali.altaher@external.thalesgroup.com>
2023-11-02 10:22:57 +0400

    adding locals, common tags

--- conditional_expressions/conditions.tf
+++ conditional_expressions/conditions.tf
@@ -1,6 +1,12 @@
 provider "aws" {
     region = var.awsregion
 }
+locals {
+  common_tags= {
+    owner = "alialtaheralhamod@gmail.com"
+    product_name= "DevOps"
+  }
+}
 # if it is a  test instance, create one t2.micro instance
 resource "aws_instance" "Dev_instance" {
     ami = var.ami
@@ -17,4 +23,9 @@ resource "aws_instance" "prd_instance" {
         Name = "prd-${count.index+1}"
         #"prd-${var.AZ[0]}-${var.mapping["key1"]}"
         }
+}
+resource "aws_instance" "test_instance" {
+    ami = var.ami
+    instance_type = var.Prd_instance_type
+    tags = local.common_tags
 }
