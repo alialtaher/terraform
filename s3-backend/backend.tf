a09ccdcd162842c729dce05b0adece8280bd9d47
Ali altaher <ali.altaher@external.thalesgroup.com>
2023-11-20 13:16:16 +0400

    lockstate file using dynamodb LockID

--- s3-backend/backend.tf
+++ s3-backend/backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket = "terraform-state-files-alialt"
     keykey = "ec2/terraform.tfstate"
-    region = "us-east-1"    
+    region = "us-east-1"
+    dynamodynamodb_table = "terraform-remote-tfstate"    
   }
 }
