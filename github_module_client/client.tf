2053a1d9daed7f879f410be95d69e39613452e7c
Ali altaher <ali.altaher@external.thalesgroup.com>
2023-11-20 11:56:06 +0400

    add ec2 module github template

---
+++ github_module_client/client.tf
@@ -0,0 +1,3 @@
+module "github_EC2_template" {
+    source = "git::https://github.com/alialtaher/terraform_EC2_template.git"
+}
--- github_module_template/main.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-}
-resource "aws_instance" "instanceX" {
-    ami= "1234"
-    instance_type = "t2.micro"
-}
